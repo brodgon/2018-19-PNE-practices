6ece45795b0c5b293fb9d50c0bb4375427b2151f
brodgon <46930464+brodgon@users.noreply.github.com>
2019-01-24 23:22:06 +0100

    Delete sum.py

--- sum.py
+++
@@ -1,6 +0,0 @@
-print("This program will perform the sum of the first 100 integers")
-
-total = 0
-for i in range(100):
-    total = total+i+1
-print('The sum of the first 100 integers is: ', total)
