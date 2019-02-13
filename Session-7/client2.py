076e7b06e19562a5084c29916a23b1ec1139c941
brodgon <blancarg2000@gmail.com>
2019-02-13 16:02:33 +0100

    Client for exercise 2 with Gema

---
+++ Session-7/client2.py
@@ -0,0 +1,23 @@
+import socket
+
+# Create a socket for communicating with the server
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Always use this parameter!
+
+print("Socket created")
+
+PORT = 8081
+IP = "212.128.253.106"
+s.connect((IP, PORT))
+
+while True:
+    mess = input("Enter a valid message: ")
+    s.send((str.encode(mess)))
+
+msg =  s.recv(2048).decode("utf-8")
+print("Message from the server")
+print(msg)
+
+s.close()
+
+print("The end")
