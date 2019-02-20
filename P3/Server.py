37161a5971f53bcf3e6ae43470747034a83b1e33
brodgon <blancarg2000@gmail.com>
2019-02-20 19:03:58 +0100

    version 2 for server

--- P3/Server.py
+++ P3/Server.py
@@ -1,27 +1,30 @@
 import socket
 from Seq import Seq
 
-PORT = 8081
-IP = "192.168.1.40"
+PORT = 8095
+IP =  "212.128.253.106"
 MAX_OPEN_REQUEST = 5
 
 
 def process_client(cs):
     # we receive the message from the client and read it in a "human way" for us to read
     mess = cs.recv(2048).decode("utf-8")
-
-    if mess == "":
+    print(mess)
+    if mess == " ":
         msg = "ALIVE"
+        print(msg)
         cs.send(str.encode(msg))
     else:
-        seq = Seq(mess)
-        msg = "We are going to work with the following sequence: "+mess
-        cs.send(str.encode(msg))
-        request = cs.recv(2048).decode("utf-8")
-        if request == 'len':
-            tl = seq.len()
-            msg1 = "Total length of the sequence introduced is "+str(tl)
-            cs.send(str.encode(msg1))
+        hello = mess.split("\n")
+        print(hello)
+        seq1 = Seq(hello[0])
+        for i in range(len(hello)):
+            if hello[i] == "len":
+                tl = seq1.len()
+                msg = "The total length is "+str(tl)
+                cs.send(str.encode(msg))
+
+
 
 
 # Create a socket for connecting to the clients
@@ -42,5 +45,4 @@ while True:
     print("Attending  client: {}".format(address))
     process_client(clientsocket)
     # --- Close the socket
-    clientsocket.close()
 
